b3839b99433c9e38ef85741033fdfe1955f1741f
Akshay Jain <theakshay.jain@gmail.com>
2022-03-30 09:08:10 +0530

    adding user icon

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,32 @@
 import style from './App.module.scss';
-import { Routes, Route, NavLink } from 'react-router-dom';
+import { Routes, Route, NavLink, useLocation } from 'react-router-dom';
 import { Home } from '../components/Home/Home';
 import { Button } from '../components/Button/Button';
 import { useDispatch } from 'react-redux';
 import { Todo } from '../components/Todo/Todo';
 import { Plus } from '../images/Plus';
+import { UserOption } from '../components/UserIcon/UserIcon';
+import Person from '../images/person.png';
 export function App() {
   const dispatch = useDispatch();
+  const location = useLocation();
+  console.log('o', location);
   return (
     <div className="app">
       <h1 className={style.header}>
         <NavLink to="/"> To Do App</NavLink>
-        <NavLink to="add">
-          <Button text="Add" icon={<Plus />} />
-        </NavLink>
+        {location.pathname === '/' ? (
+          <NavLink to="add">
+            <Button text="Add" icon={<Plus />} />
+          </NavLink>
+        ) : null}
+        <div className={style.user}>
+          <UserOption
+            invert
+            name="Simona Williams"
+            icon={<img src={Person} width="36" height="36" />}
+          />
+        </div>
       </h1>
 
       <div></div>
--- src/components/UserIcon/UserIcon.tsx
+++ src/components/UserIcon/UserIcon.tsx
@@ -2,14 +2,21 @@ import { getInitials } from '../../shared/util';
 import classes from './UserIcon.module.scss';
 interface IpUserIcon {
   name: string;
+  icon?: any;
+  size?: number;
+  invert?: boolean;
 }
 export const UserIcon = ({ name }: IpUserIcon) => {
   return <div className={classes.icon}>{getInitials(name)}</div>;
 };
-export const UserOption = ({ name }: IpUserIcon) => {
+export const UserOption = ({ name, icon, invert = false }: IpUserIcon) => {
   return (
-    <div className={classes.userOption}>
-      <UserIcon name={name} />
+    <div className={`${classes.userOption} ${invert ? classes.invert : ''}`}>
+      {icon ? (
+        <div className={classes.personIcon}>{icon}</div>
+      ) : (
+        <UserIcon name={name} />
+      )}
       <div className={classes.userOptionName}>{name}</div>
     </div>
   );

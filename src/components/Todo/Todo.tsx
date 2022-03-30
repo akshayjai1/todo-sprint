14ab259957f72ed7ffe1d1c42b50039cb6cc2809
Akshay Jain <theakshay.jain@gmail.com>
2022-03-30 08:30:16 +0530

    adding react select

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,7 +1,11 @@
+import ReactSelect from 'react-select';
+import { priority } from '../../data/priority';
+import { users } from '../../data/users';
 import { Plus } from '../../images/Plus';
 import { Button } from '../Button/Button';
 import { Control } from '../Control/Control';
 import { RowAction } from '../RowAction/RowAction';
+import { UserOption } from '../UserIcon/UserIcon';
 import style from './Todo.module.scss';
 interface IpTodo {
   edit?: boolean;
@@ -15,36 +19,43 @@ export const Todo = ({ edit = false }: IpTodo) => {
           <input type="text" placeholder="Type here" />
         </Control>
         <Control label="Priority">
-          <select name="priority" value="low" onChange={() => {}}>
-            <option value="low">low</option>
-            <option value="medium">medium</option>
-            <option value="high">high</option>
-          </select>
+          <ReactSelect
+            name="priority"
+            onChange={() => {}}
+            options={priority.map((p) => ({ label: p, value: p }))}
+          />
         </Control>
         <Control label="Due date">
           <input type="date" placeholder="Today" />
         </Control>
         <Control label="Assigned to">
-          <select name="assignee" value="low" onChange={() => {}}>
-            <option value="low">low</option>
-            <option value="medium">medium</option>
-            <option value="high">high</option>
-          </select>
+          <ReactSelect
+            isClearable
+            options={users?.map((u) => ({
+              label: <UserOption name={u.name} />,
+              value: u.id,
+            }))}
+            name="assignee"
+            // value={users[0]?.id}
+            onChange={(e) => {
+              return e;
+            }}
+          />
         </Control>
       </div>
       <div className={style.actionContainer}>
         <Button text="Cancel" onClick={() => {}} />
         <Button text="Save" onClick={() => {}} icon={<Plus />} />
       </div>
-      {!edit && (
+      {edit && (
         <>
-          <hr />
+          <hr className={style.hr} />
           <RowAction
             text="Mark this todo complete"
             actionText="Mark complete"
             onClick={() => {}}
           />
-          <hr />
+          <hr className={style.hr} />
           <RowAction
             text="Do you want to delete this todo ?"
             actionText="Delete todo"

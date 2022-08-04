4774c213bdb32a11acdd634a84e7f45d8413f9b6
Akshay Jain <theakshay.jain@gmail.com>
2022-08-04 10:48:04 +0530

    added e2e config and spec for button

--- src/components/Button/Button.cy.tsx
+++ src/components/Button/Button.cy.tsx
@@ -1,9 +1,16 @@
 /// <reference types="cypress" />
-
+import {mount} from 'cypress/react';
 import {Button} from './Button';
 
 describe('Button.cy.tsx', () => {
-  it('playground', () => {
-    cy.mount(<Button text="Button Text" />)
+  it('button component renders text properly', () => {
+    mount(<Button text="Button Text" />);
+    cy.contains("Button Text");
+  })
+  it('onClick handler of button is callable', () => {
+    const onButtonClickSpy = cy.spy().as('onButtonClickSpy');
+    mount(<Button text="Button Text" onClick={onButtonClickSpy}/>);
+    cy.contains("Button Text").click();
+    cy.get('@onButtonClickSpy').should('have.been.calledOnce');
   })
 })
